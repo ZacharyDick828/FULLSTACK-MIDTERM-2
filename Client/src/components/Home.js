02947788f15c85118e436c8a40f7176ea2ce741a
Brian Makori <bemakori@gmail.com>
2022-04-25 21:29:10 -0400

    front end

---
+++ Client/src/components/Home.js
@@ -0,0 +1,10 @@
+export default function Home () {
+  return(
+      <div className="homepage">
+          <h1>For me, electronic music is like cooking: it's a sensual organic activity where you can mix ingredients.</h1>
+
+          <h1>~Jean-Michel Jarre</h1>
+      </div>
+  )
+
+}
